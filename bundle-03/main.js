351c39c8423cb1f281a224d455e2b27f22cfa5f5
RiccardoImperato <riccardo.imperato@libero.it>
2023-11-23 16:04:26 +0100

    feat: bundle3

---
+++ bundle-03/main.js
@@ -0,0 +1,88 @@
+/*******************************************************************************
+​
+    Per questo esercizio, rispondi a queste domande:
+    1- Che cosa fa questo codice?
+    2- Sono presenti errori di sintassi?
+    3- Sono presenti errori logici?
+​
+    Inserisci i tuoi commenti con le risposte alla fine dell'esercizio
+​
+*******************************************************************************/
+
+
+// ESERCIZIO 1 (suggerimento: ci sono 6 errori)
+const cars = [
+    {
+        manufacturer: 'Ford',
+        model: 'Fiesta',
+        type: 'diesel'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'A1',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Golf',
+        type: 'benzina' // Maiuscola
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Panda',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Multipla',
+        type: 'GPL'
+    },
+    {
+        manufacturer: 'Tesla',
+        model: 'Model 3',
+        type: 'elettrico'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Polo',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Ford',
+        model: 'Kuga',
+        type: 'diesel' // Maiuscola
+    },
+    {
+        manufacturer: 'Seat',
+        model: 'Ibiza',
+        type: 'metano'
+    }, // virogola
+    {
+        manufacturer: 'Audi',
+        model: 'R8',
+        type: 'benzina'  // Maiuscola
+    },
+];
+
+const gasolineCars = cars.filter((auto) => {
+    return auto.type === 'benzina'; // return mancante
+}); // freccia e graffe
+
+const dieselCars = cars.filter((auto) => {
+    return auto.type === 'diesel'; // return mancante
+});
+
+const otherCars = cars.filter((auto) => {
+    return auto.type !== 'benzina' || auto.type !== 'diesel';
+});
+
+console.log('Auto a benzina');
+console.log('*******************************');
+console.log(gasolineCars);
+
+console.log('Auto a diesel');
+console.log('*******************************');
+console.log(dieselCars);
+
+console.log('Tutte le altre auto');
+console.log(otherCars);
